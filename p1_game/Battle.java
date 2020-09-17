100a701e6722213fb8c73d19777e58c35d49a119
kgy9466 <kgy9466@gmail.com>
2020-09-17 21:02:54 +0900

    no message

--- p1_game/Battle.java
+++ p1_game/Battle.java
@@ -22,7 +22,7 @@ public class Battle {
 		this.pointArr = new int[4];
 		this.pointTurn = new int[4];
 		for(int i = 0; i < this.pointArr.length; i++) {
-			this.pointArr[i] = (int)(Math.random() * 101) + 5;
+			this.pointArr[i] = (int)(Math.random() * 10) + 5;
 		}
 		for(int i = 0; i < this.pointTurn.length; i++) {
 			this.pointTurn[i] = (int)(Math.random() * 4);
@@ -32,16 +32,16 @@ public class Battle {
 	void battlePart() {
 		this.point = (int)(Math.random() * 101);
 		this.point1 = (int)(Math.random() * 101);
-		//this.hp = this.pointArr[this.pointTurn[0]];
+		this.hp = this.pointArr[this.pointTurn[0]];
 		this.mp = this.pointArr[this.pointTurn[1]];
-		//this.att = this.pointArr[this.pointTurn[2]];
+		this.att = this.pointArr[this.pointTurn[2]];
 		this.def = this.pointArr[this.pointTurn[3]];
 /*		Character c = new Character("유저", 100 + this.point, this.mp + this.point, 
 												10 + this.point, this.def + this.point);
 		Character1 c1 = new Character1("대전캐릭", 100 + this.point1, this.mp + this.point1, 
 												10 + this.point1, this.def + this.point1);*/
-		CharacterUser c = new CharacterUser("유저", 100, 5, 4, 3);
-		CharacterAI1 c1 = new CharacterAI1("대전캐릭", 100, 5, 4, 3);
+		CharacterUser c = new CharacterUser("유저", 100, 0, 10 + this.att + this.point, 0);
+		CharacterAI1 c1 = new CharacterAI1("대전캐릭", 100, 0, 10 + this.att + this.point1, 0);
 		System.out.println(c1.name + "를 만났습니다. 전투를 시작합니다.");
 		
 		int input = 0;
