7a88e685f6e59489f89e00a30bf491d4b902363f
kgy9466 <kgy9466@gmail.com>
2020-09-17 10:48:56 +0900

    no message

--- e_oop/RandomGame.java
+++ e_oop/RandomGame.java
@@ -6,8 +6,9 @@ public class RandomGame {
 	
 	boolean game;
 	int gameRes;
+	static int ansInput;
 	
-	final static int GAME_NUM = (int)(Math.random() * 101);
+	final static int GAME_NUM = (int)(Math.random() * 99) + 2;
 	
 	
 	RandomGame(){
@@ -17,12 +18,12 @@ public class RandomGame {
 		game = !game;
 		System.out.println(game ? "게임 시작" : "게임 종료");
 	}
-	void play(int payNum){
+	void play(int playNum){
 		if(game){
-			if(GAME_NUM == payNum){
+			if(GAME_NUM == playNum){
 				this.game = false;
 			}
-			System.out.println("잘꺼지는지 확인");
+			//System.out.println("잘꺼지는지 확인");
 		}
 	}
 	void end(){ //시스템 종료
@@ -33,16 +34,16 @@ public class RandomGame {
 		int randomVal = GAME_NUM;
 		System.out.print("랜덤 숫자 게임 시작(시작은 1)> ");
 		int startInput = ScanUtil.nextInt();
+		if(startInput != 1) {
+			randomgame.end();
+		}
+		
 		
 		while(true){
 			
-			if(startInput != 1) {
-				randomgame.end();
-			}
-			
 			System.out.print("숫자 입력> ");
-			int ansInput = ScanUtil.nextInt();
-			
+			ansInput = ScanUtil.nextInt();
+									
 			Ran ran = new Ran();
 			ran.ranNum(randomVal);
 			
@@ -50,16 +51,20 @@ public class RandomGame {
 			case 1: randomgame.game(); break;
 			
 			case 2:
-			System.out.print("숫자 입력>");
-			int reAnsInput = ScanUtil.nextInt();
-			randomgame.play(reAnsInput);
+			//System.out.print("숫자 입력1>");
+			//int reAnsInput = ScanUtil.nextInt();
+			//randomgame.play(reAnsInput);
+			randomgame.play(ansInput);
+				
 			break;
 			
 				
 			default:
+				if(ran.ranNum == ansInput) {
 				System.out.println("랜덤 숫자 값 정답 :  " + ran.ranNum);
 				System.out.println("종료되었습니다.");
 				randomgame.end();
+				}
 				break;
 			}
 
